c9dcab94140c19883298c92a2be5b578b8998bc6
Philip Basford <pjb@ecs.soton.ac.uk>
2015-03-03 14:13:57 +0000

    Now with weekend highlighting - thanks to cjg and data.southampton.ac.uk

--- www/battery.php
+++ www/battery.php
@@ -23,6 +23,43 @@
         width: 800,
         ylabel: "Voltage (V)",
         xlabel: "Date",
+ // add highlight for weekends
+        underlayCallback: function (canvas, area, g) {
+            canvas.fillStyle = "rgba(227,238,242,1.0)";
+            function highlight_period(x_start, x_end) {
+                var canvas_left_x = g.toDomXCoord(x_start);
+                var canvas_right_x = g.toDomXCoord(x_end);
+                var canvas_width = canvas_right_x - canvas_left_x;
+                canvas.fillRect(canvas_left_x, area.y, canvas_width, area.h);
+            }
+            var min_data_x = g.getValue(0, 0);
+            var max_data_x = g.getValue(g.numRows() - 1, 0);
+            var d = new Date(min_data_x);
+            var dow = d.getUTCDay();
+            var ds = d.toUTCString();
+            var w = min_data_x;
+            if (dow == 0) {
+                highlight_period(w, w + 43200000);
+            }
+            while (dow != 6) {w += 86400000;
+                d = new Date(w);
+                dow = d.getUTCDay();
+            }
+            w -= 43200000;
+            while (w < max_data_x) {
+                var start_x_highlight = w;
+                var end_x_highlight = w + 172800000;
+                if (start_x_highlight < min_data_x) {
+                    start_x_highlight = min_data_x;
+                }
+                if (end_x_highlight > max_data_x) {
+                    end_x_highlight = max_data_x;
+                }
+                highlight_period(start_x_highlight, end_x_highlight);
+                w += 604800000;
+            }
+       }
+
         }
     );
 </script>
