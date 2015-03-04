13663ddff202f45bf05c1c60c72178333a86b4e3
Philip Basford <pjb@ecs.soton.ac.uk>
2015-03-04 11:32:34 +0000

    Will output json data for the location data

---
+++ www/location_data.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("/opt/wais-sensor-server/python")
+import logging
+
+import sensordb
+
+def index(req):
+    req.content_type = "text/plain"
+    database = sensordb.WaisSensorDb("/opt/wais-sensor-server/python/db.ini")
+    data = database.get_current_locations()
+    output = "{"
+    #for line in data:
+    #    output +="\"%s\":\"%s\",\n" %(line[0], line[1])
+    #output += "}"
+    i = 0
+    while i < len(data):
+        output +=  "\"%s\":\"%s\"" % (data[i][0], data[i][1])
+        if i < (len(data) -1):
+            output += ",\n"
+        else:
+            output +="\n"
+        i+= 1
+    output += "}"
+    return output
